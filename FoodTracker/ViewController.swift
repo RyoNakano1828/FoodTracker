56ef33ff2ec0e6b61edb1b73cec4fd5570886fb2
RyoNakano <naca.11868@gmail.com>
2021-01-03 02:35:49 +0900

    テキスト、ラベル、ボタン追加

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: Propaties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameTextField.delegate = self
+    }
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: Any) {
+        mealNameLabel.text = "Default"
     }
 
+}
 
+// MARK: UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
 }
 
